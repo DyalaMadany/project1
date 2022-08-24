54a0f8c124626f9bc8d46354ccdac192b41f7db6
Mohammad Alotol <m.alotol@unitone.co>
2022-08-24 13:00:35 +0300

    stop firebase

--- lib/second_view.dart
+++ lib/second_view.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart ' as http;
+import 'package:http/http.dart' as http;
 import 'package:project1/user_model.dart';
 
 final allUsers = FutureProvider.autoDispose<List<UserModel>>((ref) async {
